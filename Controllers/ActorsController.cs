278af3380a4c8a8968e1f95b61a4e69ba860a749
aakash <aakash.jiyani@yahoo.ca>
2022-12-19 22:06:24 -0600

    Created Empty Actors controller and view

---
+++ Controllers/ActorsController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MovieTickets.Data;
+
+namespace MovieTickets.Controllers
+{
+    public class ActorsController : Controller
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ActorsController(AppDbContext context)
+        {
+            _dbContext = context;
+        }
+        public IActionResult Index()
+        {
+            var allActors = _dbContext.Actors.ToList();
+            return View(allActors);
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -11,6 +11,8 @@ builder.Services.AddDbContext<AppDbContext>(
             options => options.UseSqlServer(
             builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddScoped<AppDbContext,AppDbContext>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
